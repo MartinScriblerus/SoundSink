cbd69170ef5fc8b05390b6ec12f209f6efc27208
Matthew Reilly <matthewreilly@matthews-air.lan>
2024-11-16 10:39:19 -0600

    forgot to add page updates

--- js/next/src/app/page.tsx
+++ js/next/src/app/page.tsx
@@ -5,16 +5,22 @@ import { CssBaseline } from '@mui/material'
 import axios from 'axios';
 import { Inter } from 'next/font/google'
 import PianoIcon from '@mui/icons-material/Piano';
+import { useState } from 'react';
  
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
+
   return (
     <main className={styles.main}>
-
-
-      <div className={styles.center}>
+      <div 
+        id={'centerRoot'} 
+        style={{
+          background: 'conic-gradient(from 0deg, #ff7b7b, #ffdb4d, #76ff7a, #4d91ff, #ff7b7b)',
+          transition: 'background 0.2s ease;', /* Smooth transition */
+          willChange: 'background'
+      }} className={styles.center}>
         {/* <Image
           className={styles.logo}
           src="/next.svg"
